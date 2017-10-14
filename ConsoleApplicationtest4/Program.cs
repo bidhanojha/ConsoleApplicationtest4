5ac470e8385f4684c7b8604b1c470435d1ded9e5
bidhanojha <bidhanojha@gmail.com>
2017-10-15 01:11:39 +0200

    Add project files.

---
+++ ConsoleApplicationtest4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ControllerClass;
+
+namespace ConsoleApplicationtest4
+{
+    class ReadProgram : Program 
+    {
+        public string NewColor()
+        {
+            string _newcolor;
+            Program nP = new Program();
+            _newcolor = nP.Color();
+            Console.WriteLine(_newcolor);
+            return _newcolor;
+        }
+
+    }
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+
+            //int i;
+            //i = 10;
+            //Console.WriteLine(i);
+            //Program P = new Program();
+            //Console.WriteLine(P.Color());
+            //Console.ReadKey();
+            
+            //ReadProgram rP = new ReadProgram();
+            //Console.WriteLine(rP.NewColor());
+
+            Console.ReadKey();
+            ConsoleKeyInfo conKey = new ConsoleKeyInfo();
+            char consoleKey = conKey.KeyChar;
+            Console.WriteLine(consoleKey);
+            Console.ReadLine();
+            
+            ColorSelector cs = new ColorSelector();
+            String pickedColor = cs.ColorPick(consoleKey);
+            Console.WriteLine(pickedColor);
+            Console.ReadLine();
+            
+        }
+
+        public string Color()
+        {
+            string _color;
+            _color = "Blue";
+            return _color;
+        } 
+    }
+}
---
+++ ControllerClass/ColorSelector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerClass
+{
+    public class ColorSelector
+    {
+        public string ColorPick(char inputColor)
+        {
+            if (inputColor == 'P')
+            return "Pink";
+            else if (inputColor == 'G')
+                return "Green";
+            else
+            {
+                return "No Match";
+            }
+
+        }
+    }
+}
